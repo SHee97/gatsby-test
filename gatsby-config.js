e34315ad084815e7e3bb29bc5196c67aa8cb88cb
Ok-Cheese <starberry970310@gmail.com>
2022-12-21 15:48:21 +0900

    Improve accessibility

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,12 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+    {
       resolve: 'gatsby-plugin-typescript',
       options: {
         isTSX: true,
